e1d1b491b507bee28c247052728f91c372652b0a
ahmedawaad1804 <ahmedawaad1804@gmail.com>
2023-10-20 14:02:04 +0300

    create car widget

---
+++ ios/Cars/CarsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CarsBundle.swift
+//  Cars
+//
+//  Created by Ahmed Awaad on 20/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CarsBundle: WidgetBundle {
+    var body: some Widget {
+        Cars()
+    }
+}
